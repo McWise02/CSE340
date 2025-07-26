bdb4cf371b28b38d59fcd5bb40614766de811ca6
McWise02 <micah02.brown@gmail.com>
2025-07-26 21:07:23 +0200

    final assignment week 3

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -3,8 +3,9 @@ const express = require("express")
 const router = new express.Router() 
 const invController = require("../controllers/invController")
 const productController = require("../controllers/productController")
+const utilities = require("../utilities/")
 // Route to build inventory by classification view
-router.get("/type/:classificationId", invController.buildByClassificationId);
-router.get("/detail/:id", productController.buildByProductId);
+router.get("/type/:classificationId", utilities.handleErrors(invController.buildByClassificationId));
+router.get("/detail/:id", utilities.handleErrors(productController.buildByProductId));
 
 module.exports = router;
--- utilities/index.js
+++ utilities/index.js
@@ -91,4 +91,15 @@ Util.buildProductGrid = async function(vehicle) {
   console.log("Grid:", grid)
   return grid
 }
+
+
+
+/* ****************************************
+ * Middleware For Handling Errors
+ * Wrap other function in this for 
+ * General Error Handling
+ **************************************** */
+Util.handleErrors = fn => (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next)
+
+
 module.exports = Util
